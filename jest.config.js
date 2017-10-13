59ea7b0ed0d1bf6a8b6b870ae9e0032571659c8b
yogaboll <markus.s.englund@gmail.com>
2017-10-13 20:12:39 +0700

    add examples/dist back to the repo

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  verbose: true,
-  collectCoverage: true,
-  coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: ['./node_modules', './jest.setup.js'],
-  setupFiles: ['./jest.setup.js']
-};
